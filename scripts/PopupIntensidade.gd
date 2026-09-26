815b1ec3b920e0ebe165a0970f5884ff64e20a6c
Gabriel Normanha <gabrielnormanhasj@gmail.com>
2026-09-26 15:58:39 -0300

    Script PopUpIntensidade

--- scripts/PopupIntensidade.gd
+++ scripts/PopupIntensidade.gd
@@ -1,11 +1,28 @@
-extends Node
+extends Control
 
+@onready var slider: Range = $PainelPopup/Margens/Conteudo/SliderIntensidade
+@onready var valor_label: Label = $PainelPopup/Margens/Conteudo/ValorIntensidade
+@onready var descricao_label: Label = $PainelPopup/Margens/Conteudo/DescricaoIntensidade
+@onready var confirmar_button: Button = $PainelPopup/Margens/Conteudo/AreaConfirmar/BotaoConfirmar
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	slider.value_changed.connect(_on_slider_changed)
+	confirmar_button.pressed.connect(_on_confirmar_pressed)
+	_update_display(slider.value)
 
+func _on_slider_changed(value: float) -> void:
+	_update_display(value)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _update_display(value: float) -> void:
+	valor_label.text = "%d/10" % int(value)
+
+	if value <= 3:
+		descricao_label.text = "Leve"
+	elif value <= 7:
+		descricao_label.text = "Moderado"
+	else:
+		descricao_label.text = "Intenso"
+
+func _on_confirmar_pressed() -> void:
+	GameState.selected_intensity = str(int(slider.value))
+	
